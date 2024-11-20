e03655a40743558f341b4136d1aaebe3fd618a12
NACHENN <132915741+NACHENN@users.noreply.github.com>
2024-11-20 12:51:23 -0300

    Update prom3.js

---
+++ prom3.js
@@ -0,0 +1,16 @@
+function delay(nMilisegundos) {
+    return new Promise(resolve => setTimeoU(resolve,nMilisegundos));
+}
+delay(6000).then(() => {
+console.log("espera completada");
+});
+delay(2000) .then (() => {
+console.log("segunda espera completada")
+} );
+    then ()  
+ delay (4000)
+    console.log("tercera carga completada")
+
+    then ()
+delay (6000)
+    console.log("cuarta carga completada")
